44b3c632d9c56298b146ae9a1ceafa5df306837e
Grace Ligit Nuh <56198370+graceln@users.noreply.github.com>
2022-11-12 10:57:00 +0800

    admin

---
+++ admin.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <link rel="icon" href="./image/dashboard.ico">
+
+</head>
+<body>
+
+    <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="las la-mug-hot"></span><span>Ahay Coffee</span></h2>
+        </div>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="admin.php" class="active"><span class="las la-igloo"></span>
+                    <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href="data-menu.php"><span class="las la-coffee"></span>
+                    <span>Data Menu</span></a>
+                </li>
+                <li>
+                    <a href="data-supplier.php"><span class="las la-receipt"></span>
+                    <span>Data Supplier</span></a>
+                </li>
+                <li>
+                    <a href="data-karyawan.php"><span class="las la-users"></span>
+                    <span>Data Karyawan</span></a>
+                </li>
+                <li>
+                    <a href="data-pembeli.php"><span class="las la-users"></span>
+                    <span>Data Pembeli</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-content">
+        <header>
+            <h2>
+                <label for="nav-toggle">
+                    <span class="las la-bars"></span>
+                </label>
+                Dashboard
+            </h2>
+
+            <div class="user-wrapper">
+                <img src="image/pic-1.png" width="40px" height="40px" alt="">
+                <div>
+                    <h4>John Doe</h4>
+                    <small>Admin</small>
+                </div>
+            </div>
+
+            <div class="logout-wrapper">
+                <a href="index.php"><span class="las la-power-off"></span></a>
+            </div>
+        </header>
+
+        <main>
+            <div class="cards">
+                <div class="card-single">
+                    <div>
+                        <h1>54</h1>
+                        <span>Pembeli</span>
+                    </div>
+                    <div>
+                        <span class="las la-users"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>20</h1>
+                        <span>Menu</span>
+                    </div>
+                    <div>
+                        <span class="las la-coffee"></span>
+                    </div>
+                </div>
+
+
+                <div class="card-single">
+                    <div>
+                        <h1>124</h1>
+                        <span>Orders</span>
+                    </div>
+                    <div>
+                        <span class="las la-shopping-bag"></span>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div>
+                        <h1>$6k</h1>
+                        <span>Income</span>
+                    </div>
+                    <div>
+                        <span class="lab la-google-wallet"></span>
+                    </div>
+                </div>
+            </div>
+
+        </mnain>
+
+    </div>
+
+
+</body>
+</html>
