dce5f37663e0f868310a4bccd699752ff6141cef
Grace Ligit Nuh <56198370+graceln@users.noreply.github.com>
2022-11-12 11:04:48 +0800

    tambah supplier

---
+++ tambah-supplier.php
@@ -0,0 +1,123 @@
+<?php 
+include 'koneksi.php';
+
+if(isset($_POST['submit'])){
+    $NAMA_SUPP = htmlspecialchars($_POST['NAMA_SUPP']);
+    $ALAMAT_SUPP = htmlspecialchars($_POST['ALAMAT_SUPP']);
+    $KOMODITAS = htmlspecialchars($_POST['KOMODITAS']);
+  
+    $sql = "INSERT INTO supplier (ID_SUPPLIER,NAMA_SUPP,ALAMAT_SUPP,KOMODITAS) 
+    VALUES ('','$NAMA_SUPP','$ALAMAT_SUPP','$KOMODITAS')";
+
+    $result = mysqli_query($connection, $sql);
+
+
+    if($result){
+        echo "<script>alert('Data berhasil ditambahkan!');window.location='data-supplier.php';</script>";  
+    } else {
+        echo "<script>alert('Data gagal ditambahkan!');window.location='data-supplier.php';</script>";  
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <title>Tambah Data Supplier</title>
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/input.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <link rel="icon" href="./image/supplier.ico">
+</head>
+<body>
+
+    <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="las la-mug-hot"></span><span>Ahay Coffee</span></h2>
+        </div>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="admin.php"><span class="las la-igloo"></span>
+                    <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href="data-menu.php" class="active"><span class="las la-coffee"></span>
+                    <span>Data Menu</span></a>
+                </li>
+                <li>
+                    <a href="data-supplier.php"><span class="las la-receipt"></span>
+                    <span>Data Supplier</span></a>
+                </li>
+                <li>
+                    <a href="data-karyawan.php"><span class="las la-users"></span>
+                    <span>Data Karyawan</span></a>
+                </li>
+                <li>
+                    <a href="data-pembeli.php"><span class="las la-users"></span>
+                    <span>Data Pembeli</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-content">
+        <header>
+            <h2>
+                <label for="nav-toggle">
+                    <span class="las la-bars"></span>
+                </label>
+                Data Supplier
+            </h2>
+
+            <div class="user-wrapper">
+                <img src="image/pic-1.png" width="40px" height="40px" alt="">
+                <div>
+                    <h4>John Doe</h4>
+                    <small>Admin</small>
+                </div>
+            </div>
+
+            <div class="logout-wrapper">
+                <a href="index.php"><span class="las la-power-off"></span></a>
+            </div>
+        </header>
+
+        <!-- Ini Isi Konten -->
+        <main>
+            <div class="wrapper">
+            <h2>Tambah Data Supplier</h2>
+            <hr>
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="">Nama Supplier</label>
+                        <input type="text" name="NAMA_SUPP" required="required" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Alamat Supplier</label>
+                        <input type="text" name="ALAMAT_SUPP" required="required" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Komoditas</label>
+                        <input type="text" name="KOMODITAS" required="required" autocomplete="off">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <button type="submit" name="submit" value="submit" class="btn">Tambah</button>
+                        <button class="kembali"><a href="data-supplier.php" class="kembali">Kembali</a></button>
+                    </div>
+                </form>
+            </div>
+        </main>
+        <!-- Akhir Isi Konten -->
+    </div>
+
+</body>
+</html>
