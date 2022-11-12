9351120d20e00859cb89139ece542e8118a03dd8
Grace Ligit Nuh <56198370+graceln@users.noreply.github.com>
2022-11-12 10:58:28 +0800

    data menu

---
+++ data-menu.php
@@ -0,0 +1,142 @@
+<?php 
+// error_reporting(0);
+include 'koneksi.php';
+// session_start();
+// if (!isset($_SESSION['signin'])) {
+//     header("Location: signin.php");
+//     exit;
+// }
+$cek = "";
+if (isset($_POST['cari'])) {
+    $cari = $_POST['keyword'];
+    $data = mysqli_query($connection, "SELECT * FROM menu WHERE nama_menu LIKE '%$cari%' OR jenis_menu LIKE '%$cari%' OR harga_menu LIKE '%$cari%'");
+    $cek = mysqli_num_rows($data);
+} else {
+    $data = mysqli_query($connection, "SELECT * FROM menu ORDER BY ID_MENU asc");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <title>Data Menu</title>
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/cari.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <link rel="icon" href="./image/ahay.ico">
+</head>
+<body>
+
+    <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="las la-mug-hot"></span><span>Ahay Coffee</span></h2>
+        </div>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="admin.php"><span class="las la-igloo"></span>
+                    <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href="data-menu.php" class="active"><span class="las la-coffee"></span>
+                    <span>Data Menu</span></a>
+                </li>
+                <li>
+                    <a href="data-supplier.php"><span class="las la-receipt"></span>
+                    <span>Data Supplier</span></a>
+                </li>
+                <li>
+                    <a href="data-karyawan.php"><span class="las la-users"></span>
+                    <span>Data Karyawan</span></a>
+                </li>
+                <li>
+                    <a href="data-pembeli.php"><span class="las la-users"></span>
+                    <span>Data Pembeli</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-content">
+        <header>
+            <h2>
+                <label for="nav-toggle">
+                    <span class="las la-bars"></span>
+                </label>
+                Data Menu
+            </h2>
+
+            <div class="user-wrapper">
+                <img src="image/pic-1.png" width="40px" height="40px" alt="">
+                <div>
+                    <h4>John Doe</h4>
+                    <small>Admin</small>
+                </div>
+            </div>
+
+            <div class="logout-wrapper">
+                <a href="index.php"><span class="las la-power-off"></span></a>
+            </div>
+        </header>
+
+        <!-- Ini Isi Konten -->
+        <main>
+        <div class="content">
+                <h2>DATA MENU</h2>
+                <hr><br>
+                <button class="btn"><a href="tambah-menu.php" class="btn">Tambah</a></button><br><br>
+                    <form action="" method="POST">
+                        <input  class="search"type="text" name="keyword" id="live-search" placeholder="Cari" autocomplete="off">
+                        <button type="submit" name="cari" id="keyword" class="button">Cari</button>
+                    </form>
+                    <div id="searchresult"></div>
+                    <table class="table">
+                        <thead>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Jenis</th>
+                            <th>Harga</th>                            
+                            <th>Gambar</th>
+                            <th>Aksi</th>
+                        </thead>
+                        <?php if($cek == "0") { ?>
+                        <tr>
+                            <td colspan="7" style="text-align:center;">No data available in table</td>
+                        </tr>
+                        <?php } ?>
+                        <tbody>
+                            <?php 
+                            include 'koneksi.php';
+                            $no = 1;
+                            while($d = mysqli_fetch_array($data)){ ?>
+                                <tr>
+                                    <td><?php echo $no++; ?></td>
+                                    <td><?php echo $d['NAMA_MENU']; ?></td>
+                                    <td><?php echo $d['JENIS_MENU']; ?></td>
+                                    <td><?php echo $d['HARGA_MENU']; ?></td>                                    
+                                    <td><img style="width: 40px;" src="gambar/<?php echo $d['GAMBAR']; ?>"></td>
+                                    <td>
+                                        <button class="edit"><a href="ubah-menu.php?ID_MENU=<?php echo $d['ID_MENU']; ?>" class="edit">Ubah</a></button>
+                                        <button class="hapus"><a href="hapus-menu.php?ID_MENU=<?php echo $d['ID_MENU']; ?>" onclick="return confirm('Anda yakin hapus data ini?')"
+                                        class="hapus">Hapus</a></button>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </tbody>
+            </table>
+        </div>
+        </main>
+        <!-- Akhir Isi Konten -->
+
+    </div>
+
+
+</body>
+</html>
