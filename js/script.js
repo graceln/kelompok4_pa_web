a2e58258d29a1ac9ebfd1ffe7888e0a6988a6c54
Grace Ligit Nuh <56198370+graceln@users.noreply.github.com>
2022-11-12 10:56:21 +0800

    js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+
+const checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+    navbar.classList.remove('active');
+}
