5dbddb4c4f77e7a986a7587001931a6a38628e08
Muhammad Syarafi Al Fasa <113402266+SyarafiAlFasa@users.noreply.github.com>
2022-11-14 22:45:40 +0800

    ubah karyawan

--- ubah-karyawan.php
+++ ubah-karyawan.php
@@ -84,7 +84,7 @@ if(isset($_POST['submit'])){
             </div>
 
             <div class="logout-wrapper">
-                <a href="index.php"><span class="las la-power-off"></span></a>
+                <a href="logout.php"><span class="las la-power-off"></span></a>
             </div>
         </header>
 
