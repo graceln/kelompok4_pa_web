bb248cc12a014b4dd8aa99f5100d6c26e606344e
Muhammad Syarafi Al Fasa <113402266+SyarafiAlFasa@users.noreply.github.com>
2022-11-14 22:46:26 +0800

    ubah supplier

--- ubah-supplier.php
+++ ubah-supplier.php
@@ -85,7 +85,7 @@ if(isset($_POST['submit'])){
             </div>
 
             <div class="logout-wrapper">
-                <a href="index.php"><span class="las la-power-off"></span></a>
+                <a href="logout.php"><span class="las la-power-off"></span></a>
             </div>
         </header>
 
