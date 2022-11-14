e3ee8449a40aee303079fa85e331708ba6750316
Muhammad Syarafi Al Fasa <113402266+SyarafiAlFasa@users.noreply.github.com>
2022-11-14 20:35:20 +0800

    ubah supplier

--- ubah-supplier.php
+++ ubah-supplier.php
@@ -48,11 +48,11 @@ if(isset($_POST['submit'])){
                     <span>Dashboard</span></a>
                 </li>
                 <li>
-                    <a href="data-menu.php" class="active"><span class="las la-coffee"></span>
+                    <a href="data-menu.php"><span class="las la-coffee"></span>
                     <span>Data Menu</span></a>
                 </li>
                 <li>
-                    <a href="data-supplier.php"><span class="las la-receipt"></span>
+                    <a href="data-supplier.php" class="active"><span class="las la-receipt"></span>
                     <span>Data Supplier</span></a>
                 </li>
                 <li>
@@ -79,7 +79,7 @@ if(isset($_POST['submit'])){
             <div class="user-wrapper">
                 <img src="image/pic-1.png" width="40px" height="40px" alt="">
                 <div>
-                    <h4>John Doe</h4>
+                    <h4>Admin</h4>
                     <small>Admin</small>
                 </div>
             </div>
